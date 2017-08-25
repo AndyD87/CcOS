302d8365c68ac4b66bb7b120ce4d89ce55f52f77
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2017-08-25 20:21:34 +0200

    Versioning libraries with rc Files. Build-Output fixed to one directory, no Debug Release RelWithDebug MinSizeRel subdirectories. Increasing Comments in CMake Files.

--- CMakeConfig/Toolchains/MSVC.cmake
+++ CMakeConfig/Toolchains/MSVC.cmake
@@ -1,39 +1,114 @@
-###################
-# Compiler Settings
-###################
+################################################################################
+# Compiler Settings for MSVC
+################################################################################
 MESSAGE("Configuration for Windows Platform is called\n")
 
+set(CCOS_LINK_TYPE_RUNTIME STATIC)
+set(CCOS_BUILD_ARCH Unknown)
+
+################################################################################
+# Setup default Variables if not previously set
+################################################################################
+if(NOT CCOS_LINK_TYPE)
+    set(CCOS_LINK_TYPE SHARED)
+endif()
+
+if(NOT CCOS_LINK_TYPE_RUNTIME)
+    set(CCOS_LINK_TYPE_RUNTIME STATIC)
+endif()
+
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE "Release")
+endif()
+
+################################################################################
+# Set common compiler settings: 
+#  - default WarningLevel 4
+#  - static runtime if required
+################################################################################
 MESSAGE("Compiler: ${CMAKE_C_COMPILER_ID}")
 set ( CompilerFlags
         CMAKE_CXX_FLAGS
         CMAKE_CXX_FLAGS_DEBUG
         CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_CXX_FLAGS_RELWITHDEBINFO
+        CMAKE_CXX_FLAGS_MINSIZEREL
         CMAKE_C_FLAGS
         CMAKE_C_FLAGS_DEBUG
         CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_RELWITHDEBINFO
+        CMAKE_C_FLAGS_MINSIZEREL
     )
+# Enable Warning Level 4
 foreach(CompilerFlag ${CompilerFlags})
-  string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+  if(${CompilerFlag} MATCHES "/W[0-4]")
+    string(REPLACE "/W0" "/W4" ${CompilerFlag} "${${CompilerFlag}}")
+    string(REPLACE "/W1" "/W4" ${CompilerFlag} "${${CompilerFlag}}")
+    string(REPLACE "/W2" "/W4" ${CompilerFlag} "${${CompilerFlag}}")
+    string(REPLACE "/W3" "/W4" ${CompilerFlag} "${${CompilerFlag}}")
+  else()
+    set(${CompilerFlag} "${${CompilerFlag}} /W4")
+  endif()
+  if(${CCOS_LINK_TYPE_RUNTIME} STREQUAL "STATIC")
+    string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+  endif()
 endforeach()
 
-# Enable Warning Level 4
-if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
-  string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-else()
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-endif()
-if(CMAKE_C_FLAGS MATCHES "/W[0-4]")
-  string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
-else()
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
-endif()
+################################################################################
+# Set common linker flags: 
+#  - subsystem console 
+################################################################################
+set ( CompilerFlags
+        CMAKE_EXE_LINKER_FLAGS
+        CMAKE_EXE_LINKER_FLAGS_DEBUG
+        CMAKE_EXE_LINKER_FLAGS_RELEASE
+        CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO
+        CMAKE_EXE_LINKER_FLAGS_MINSIZEREL
+    )
+foreach(CompilerFlag ${CompilerFlags})
+  set(${CompilerFlag} " ${${CompilerFlag}} /SUBSYSTEM:CONSOLE")
+endforeach()
 
-if(NOT CCOS_LINK_TYPE)
-    set(CCOS_LINK_TYPE SHARED)
-endif()
-if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Release")
-endif()
+################################################################################
+# Change output targets for Windows
+################################################################################
+foreach( OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES} )
+    string( TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG )
+    set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} )
+    set( CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} )
+    set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY} )
+endforeach( OUTPUTCONFIG CMAKE_CONFIGURATION_TYPES )
 
-#Set default subsystem to console
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /SUBSYSTEM:CONSOLE ")
+# Try to get target platform by MSVC_C_ARCHITECTURE_ID
+if(MSVC_C_ARCHITECTURE_ID)
+  if("${MSVC_C_ARCHITECTURE_ID}" STREQUAL "X86")
+    set(CCOS_BUILD_ARCH x86)
+  elseif("${MSVC_C_ARCHITECTURE_ID}" STREQUAL "x64")
+    set(CCOS_BUILD_ARCH x64)
+  else()
+    message(FATAL_ERROR "Unknown architecture in ${MSVC_C_ARCHITECTURE_ID}")
+  endif()
+# Try to get target platform by CMAKE_GENERATOR_PLATFORM
+elseif(CMAKE_GENERATOR_PLATFORM)
+  if("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "win32")
+    set(CCOS_BUILD_ARCH x86)
+  elseif("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64")
+    set(CCOS_BUILD_ARCH x64)
+  else()
+    message(FATAL_ERROR "Unknown architecture in ${CMAKE_GENERATOR_PLATFORM}")
+  endif()
+else()
+  include(CheckSymbolExists)
+  check_symbol_exists("_M_AMD64" "" ARCH_X64)
+  if(NOT ARCH_X64)
+    check_symbol_exists("_M_IX86" "" ARCH_X86)
+  endif(NOT ARCH_X64)
+  
+  if(ARCH_X64)
+    set(CCOS_BUILD_ARCH x64)
+  elseif(ARCH_X86)
+    set(CCOS_BUILD_ARCH x86)
+  else()
+    message(FATAL_ERROR "Unknown architecture")
+  endif()
+endif()
--- CcOS.cmake
+++ CcOS.cmake
@@ -1,8 +1,15 @@
-include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/Macros.cmake )
+################################################################################
+# Setup Globals
+################################################################################
+if(CMAKE_PROJECT_NAME AND
+   NOT ${CMAKE_PROJECT_NAME} EQUAL "CcOS")
+  include( ${CMAKE_CURRENT_SOURCE_DIR}/VERSION.cmake )
+  include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/Macros.cmake )
+endif()
 
-###################
+################################################################################
 # Include Config File if config is set
-###################
+################################################################################
 if(DEFINED CONFIGFILE)
   include( ${CONFIGFILE} )
 else()
@@ -20,15 +27,28 @@ else()
   endif()
 endif()
 
-###################
+################################################################################
+# Load Compiler Settings depending on Compiler Type
+################################################################################
+if( APPLE )
+  include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/Toolchains/Apple.cmake)
+elseif( DEFINED MSVC )
+  set(CMAKE_BUILD_TYPE "Release")
+  include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/Toolchains/MSVC.cmake)
+else( DEFINED GCC )
+  include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/Toolchains/GCC.cmake )
+endif()
+
+################################################################################
 # Precheck all configurations and load thirdparty if required
-###################
+################################################################################
 include( ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig/ConfigCheck.cmake )
 
-###################
+################################################################################
 # Add CcOS to compile
 # Add all CcLib Objects to build-List:
-###################
+################################################################################
+add_subdirectory( ${CMAKE_CURRENT_SOURCE_DIR}/ThirdParty/ )
 add_subdirectory( ${CMAKE_CURRENT_SOURCE_DIR}/CcKernel/ )
 add_subdirectory( ${CMAKE_CURRENT_SOURCE_DIR}/CcGui/ )
 add_subdirectory( ${CMAKE_CURRENT_SOURCE_DIR}/CcNetwork/ )
