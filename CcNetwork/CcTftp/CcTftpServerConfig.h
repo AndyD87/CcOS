f20758e0177ea9e1a74744658ec91a40de9366e9
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2017-11-14 22:16:16 +0100

    Required files added

---
+++ CcNetwork/CcTftp/CcTftpServerConfig.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @page      CcTftp
+ * @subpage   CcTftpServerConfig
+ *
+ * @page      CcTftpServerConfig
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web: http://coolcow.de
+ * @version   0.01
+ * @date      2016-04
+ * @par       Language   C++ ANSI V3
+ * @brief     Class CcTftpServerConfig
+ */
+#ifndef CcTftpServerConfig_H_
+#define CcTftpServerConfig_H_
+
+#include "CcBase.h"
+#include "CcTftp.h"
+
+/**
+ * @brief Button for GUI Applications
+ */
+class CcTftpSHARED CcTftpServerConfig
+{
+public:
+  /**
+   * @brief Constructor
+   * @param Port: Port on wich the Server should listen.
+   *              Default Port is set to 27521
+   */
+  CcTftpServerConfig();
+
+  /**
+   * @brief Destructor
+   */
+  ~CcTftpServerConfig( void );
+
+  uint16 getPort() const
+    { return m_uiPort; }
+  void setPort(uint16 uiPort)
+    { m_uiPort = uiPort; }
+  
+  const CcString& getRootDir() const
+    { return m_sRootDir; }
+  void setRootDir(const CcString& sRootDir)
+    { m_sRootDir = sRootDir; }
+
+private:
+  uint16      m_uiPort;   //!< Port where Socket is listen on.
+  CcString    m_sRootDir; //!< Root-Dir of Server
+};
+
+#endif /* CcTftpServerConfig_H_ */
