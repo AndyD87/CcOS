b1916a4898069b49cbd9de6e10e7fd1c947be19a
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2018-12-12 10:18:01 +0100

    eclipse project dir outside source dir

--- Tools/CmakeSTM32F4Discovery.bat
+++ Tools/CmakeSTM32F4Discovery.bat
@@ -1,13 +1,6 @@
+SET TOOLS_DIR=%cd%
 cd ..
-mkdir Solution.ECL
-cd Solution.ECL
-cmake -G "Eclipse CDT4 - Unix Makefiles" ../ -DCCOS_BOARD=CMakeConfig/Boards/STM32F4Discovery -DCMAKE_BUILD_TYPE=Release
-cd ..
-cd Tools
-
-cd ..
-mkdir Solution.CBX
-cd Solution.CBX
-cmake -G "CodeBlocks - Ninja" ../ -DCCOS_BOARD=CMakeConfig/Boards/STM32F4Discovery -DCMAKE_BUILD_TYPE=Release
-cd ..
-cd Tools
+mkdir "%cd%.Solution.ECL"
+cd "%cd%.Solution.ECL"
+cmake -G "Eclipse CDT4 - Unix Makefiles" "%TOOLS_DIR%/.." -DCMAKE_ECLIPSE_VERSION=4.9 -DCCOS_BOARD=CMakeConfig/Boards/STM32F4Discovery -DCMAKE_BUILD_TYPE=Release
+cd "%TOOLS_DIR%"
