5f8f8c94e2200db67d38cbf76bfc593ea9f606cc
BuildSystem <BuildSystem@adirmeier.de>
2018-12-19 16:26:34 +0100

    Driver for STM32F4Discovery Board created

---
+++ CcKernel/Driver/Board/ST/STM32F4Discovery/STM32F4DiscoveryDriver.h
@@ -0,0 +1,53 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @page      STM32F4Discovery
+ * @subpage   STM32F4DiscoveryDriver
+ *
+ * @page      STM32F4DiscoveryDriver
+ * @author    Andreas Dirmeier
+ * @par       Language: C++11
+ */
+
+#ifndef _STM32F4DiscoveryDriver_H_
+#define _STM32F4DiscoveryDriver_H_
+
+#include "STM32F4Discovery.h"
+#include "CcDriver.h"
+
+class CcByteArray;
+
+/**
+ * @brief Generate SM32F407V CPU Device
+ */
+class STM32F4DiscoveryDriver : public CcDriver
+{
+public:
+  /**
+   * @brief Constructor
+   */
+  STM32F4DiscoveryDriver( void );
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~STM32F4DiscoveryDriver( void );
+
+  virtual CcStatus entry(void) override;
+};
+
+#endif /* _STM32F4DiscoveryDriver_H_ */
