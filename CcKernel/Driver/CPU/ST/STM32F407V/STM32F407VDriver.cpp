9b4448c16a8c027649e2a5288c231a3a95275682
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-02-26 13:57:36 +0100

    Original STM32F4Discovery linker script: STM32F4/STM32F407VGTx_FLASH.ld

--- CcKernel/Driver/CPU/ST/STM32F407V/STM32F407VDriver.cpp
+++ CcKernel/Driver/CPU/ST/STM32F407V/STM32F407VDriver.cpp
@@ -31,27 +31,7 @@
 #include "stm32f4xx_hal_rcc.h"
 #include "stm32f4xx_hal_pwr_ex.h"
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (HSE)
-  *            SYSCLK(Hz)                     = 168000000
-  *            HCLK(Hz)                       = 168000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 4
-  *            APB2 Prescaler                 = 2
-  *            HSE Frequency(Hz)              = 8000000
-  *            PLL_M                          = 8
-  *            PLL_N                          = 336
-  *            PLL_P                          = 2
-  *            PLL_Q                          = 7
-  *            VDD(V)                         = 3.3
-  *            Main regulator output voltage  = Scale1 mode
-  *            Flash Latency(WS)              = 5
-  * @param  None
-  * @retval None
-  */
-static void SystemClock_Config(void)
+void STM32F407VDriver::SystemClock_Config()
 {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_OscInitTypeDef RCC_OscInitStruct;
