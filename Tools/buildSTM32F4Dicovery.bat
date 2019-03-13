01281bf7ca1ed66f7552faa2c0f00239c1a0a5e4
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-03-13 08:54:07 +0100

    Update scripts for building generic

--- Tools/buildSTM32F4Dicovery.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-mkdir Solution
-cd Solution
-cmake -G "Unix Makefiles" ../ -DCCOS_BOARD=CMakeConfig/Boards/STM32F4Discovery/Config.cmake -DCMAKE_BUILD_TYPE=Release
-make
-cd ..
-cd Tools
--- Tools/buildSTM32F4Dicovery.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-mkdir Solution
-cd Solution
-cmake ../ -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=CMakeConfig/Toolchains/Boards/STM32F4Discovery/Toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=CMakeConfig/Toolchains/arm-none-eabi/make-3.81-bin/bin/make.exe
-make
-cd ..
-cd Tools
