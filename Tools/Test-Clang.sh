db666c962f1acfa94b24a934be6fbe9943bdbb6b
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-03-13 14:15:20 +0100

    Execute Tests only if compiler exists

--- Tools/Test-Clang.sh
+++ Tools/Test-Clang.sh
@@ -2,57 +2,96 @@ CWD=$(pwd)
 CPU=$(nproc)
 
 # Setup Clang
-CLANG_VERSION=-7
+
+CLANG_VERSION=-5.0
 export CC=clang$CLANG_VERSION
 export CXX=clang++$CLANG_VERSION
-
-rm -rf Solution
-rm -rf Output
-mkdir Solution
-cd Solution
-
-cmake ../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
-if [ $? -ne 0 ]
+if [ -x "$(command -v $CC)" ]
 then
-    exit -1
+    echo "CLANG_VERSION=$CLANG_VERSION"
+else
+    CLANG_VERSION=-6
+    export CC=clang$CLANG_VERSION
+    export CXX=clang++$CLANG_VERSION
+    if [ -x "$(command -v $CC)" ]
+    then
+    echo "CLANG_VERSION=$CLANG_VERSION"
+    else
+        CLANG_VERSION=-7
+        export CC=clang$CLANG_VERSION
+        export CXX=clang++$CLANG_VERSION
+        if [ -x "$(command -v $CC)" ]
+        then
+            echo "CLANG_VERSION=$CLANG_VERSION"
+        else
+            # For future
+            CLANG_VERSION=-8
+            export CC=clang$CLANG_VERSION
+            export CXX=clang++$CLANG_VERSION
+            if [ -x "$(command -v $CC)" ]
+            then
+                echo "CLANG_VERSION=$CLANG_VERSION"
+            else
+                echo "CLANG not found test without version"
+                export CC=clang
+                export CXX=clang++
+            fi
+        fi
+    fi
 fi
 
-make -j $CPU
-if [ $? -ne 0 ]
+if [ -x "$(command -v $CC)" ]
 then
-    exit -1
+    rm -rf Solution
+    rm -rf Output
+    mkdir Solution
+    cd Solution
+
+    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make -j $CPU
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make test
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    cd $CWD
+    rm -rf Solution
+    rm -rf Output
+    mkdir Solution
+    cd Solution
+
+    cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make -j $CPU
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make test
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    cd $CWD
+    rm -rf Solution
+    rm -rf Output
+else
+    echo $CC
 fi
-
-make test
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-cd $CWD
-rm -rf Solution
-rm -rf Output
-mkdir Solution
-cd Solution
-
-cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-make -j $CPU
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-make test
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-cd $CWD
-rm -rf Solution
-rm -rf Output
--- Tools/Test-Gcc.sh
+++ Tools/Test-Gcc.sh
@@ -5,53 +5,56 @@ CPU=$(nproc)
 export CC=gcc
 export CXX=g++
 
-rm -rf Solution
-rm -rf Output
-mkdir Solution
-cd Solution
-
-cmake ../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
-if [ $? -ne 0 ]
+if [ -x "$(command -v $CC)" ]
 then
-    exit -1
+    rm -rf Solution
+    rm -rf Output
+    mkdir Solution
+    cd Solution
+
+    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make -j $CPU
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make test
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    cd $CWD
+    rm -rf Solution
+    rm -rf Output
+    mkdir Solution
+    cd Solution
+
+    cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make -j $CPU
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    make test
+    if [ $? -ne 0 ]
+    then
+        exit -1
+    fi
+
+    cd $CWD
+    rm -rf Solution
+    rm -rf Output
 fi
-
-make -j $CPU
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-make test
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-cd $CWD
-rm -rf Solution
-rm -rf Output
-mkdir Solution
-cd Solution
-
-cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr -DCC_OUTPUT_DIR=$CWD/Output -DCC_WARNING_AS_ERROR=TRUE
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-make -j $CPU
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-make test
-if [ $? -ne 0 ]
-then
-    exit -1
-fi
-
-cd $CWD
-rm -rf Solution
-rm -rf Output
