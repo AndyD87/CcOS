a08399aaa03ae44ce2e18cd3de030b17fc041860
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-04-15 12:45:45 +0200

    Remove exceptions to save flash memory. CcOS does not use exceptions!

--- CMakeConfig/CPU/ST/STM32F4/Config.cmake
+++ CMakeConfig/CPU/ST/STM32F4/Config.cmake
@@ -2,7 +2,7 @@ message("- CPU: STM32F4 @ ${CMAKE_CURRENT_LIST_FILE}")
 
 set(CMAKE_SYSTEM_PROCESSOR cortex-m4 CACHE INTERNAL "")
 set(CMAKE_C_FLAGS   "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fno-threadsafe-statics -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g -fabi-version=0 -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics"  CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os -fno-exceptions -fmessage-length=0 -fsigned-char -ffunction-sections -fno-threadsafe-statics -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g -fabi-version=0 -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics"  CACHE INTERNAL "")
 
 set(CMAKE_EXE_LINKER_FLAGS    "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16  -T \"${CMAKE_CURRENT_LIST_DIR}/STM32F407VGTx_FLASH.ld\" -Xlinker --gc-sections -Wl,-Map=STM32F4.map" CACHE INTERNAL "")
 
@@ -10,7 +10,6 @@ set(CC_EXECUTABLE_SUFFIX ".elf" CACHE INTERNAL "")
   
 add_definitions(-DARM_MATH_CM4 -DUSE_FULL_ASSERT -DSTM32F407xx)
 
-
-set(CCOS_DRIVER_CPU    					  TRUE CACHE INTERNAL "")
-set(CCOS_DRIVER_CPU_ST 					  TRUE CACHE INTERNAL "")
-set(CCOS_DRIVER_CPU_ST_STM32F407V         TRUE CACHE INTERNAL "")
+set(CCOS_DRIVER_CPU    					    TRUE CACHE INTERNAL "")
+set(CCOS_DRIVER_CPU_ST 					    TRUE CACHE INTERNAL "")
+set(CCOS_DRIVER_CPU_ST_STM32F407V   TRUE CACHE INTERNAL "")
