40fc92680448fe0d7bda869eaa6bbd3900cc64d5
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-04-15 14:52:10 +0200

    Program size reduced by changing gcc compile flags. It's not tested yet

--- CMakeConfig/CPU/ST/STM32F3/Config.cmake
+++ CMakeConfig/CPU/ST/STM32F3/Config.cmake
@@ -1,10 +1,10 @@
 message("- CPU: STM32F2 @ ${CMAKE_CURRENT_LIST_FILE}")
 
 set(CMAKE_SYSTEM_PROCESSOR cortex-m4 CACHE INTERNAL "")
-set(CMAKE_C_FLAGS   "-mcpu=cortex-m4 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -Os -fno-exceptions -fmessage-length=0 -fsigned-char -ffunction-sections -fno-threadsafe-statics -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g -fabi-version=0 -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics"  CACHE INTERNAL "")
+set(CMAKE_C_FLAGS   "-mcpu=cortex-m4 -mthumb -Os --specs=nosys.specs -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra -g" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -Os --specs=nosys.specs -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra -g -fno-exceptions -fno-rtti -fno-threadsafe-statics -fabi-version=0 -fno-rtti -fno-use-cxa-atexit"  CACHE INTERNAL "")
 
-set(CMAKE_EXE_LINKER_FLAGS    "-mcpu=cortex-m4 -mthumb -T \"${CMAKE_CURRENT_LIST_DIR}/STM32F303VCTx_FLASH.ld\" -Xlinker --gc-sections -Wl,-Map=STM32F3.map" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS    "-mcpu=cortex-m4 -mthumb --specs=nano.specs -T \"${CMAKE_CURRENT_LIST_DIR}/STM32F303VCTx_FLASH.ld\" -Xlinker --gc-sections -Wl,-Map=STM32F3.map" CACHE INTERNAL "")
 
 set(CC_EXECUTABLE_SUFFIX ".elf" CACHE INTERNAL "")
   
--- CMakeConfig/CPU/ST/STM32F4/Config.cmake
+++ CMakeConfig/CPU/ST/STM32F4/Config.cmake
@@ -1,10 +1,10 @@
 message("- CPU: STM32F4 @ ${CMAKE_CURRENT_LIST_FILE}")
 
 set(CMAKE_SYSTEM_PROCESSOR cortex-m4 CACHE INTERNAL "")
-set(CMAKE_C_FLAGS   "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os -fno-exceptions -fmessage-length=0 -fsigned-char -ffunction-sections -fno-threadsafe-statics -fdata-sections -ffreestanding -Wall -Wextra --specs=nosys.specs -g -fabi-version=0 -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics"  CACHE INTERNAL "")
+set(CMAKE_C_FLAGS   "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os --specs=nosys.specs -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra -g" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Os --specs=nosys.specs -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra -g -fno-exceptions -fno-rtti -fno-threadsafe-statics -fabi-version=0 -fno-rtti -fno-use-cxa-atexit"  CACHE INTERNAL "")
 
-set(CMAKE_EXE_LINKER_FLAGS    "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16  -T \"${CMAKE_CURRENT_LIST_DIR}/STM32F407VGTx_FLASH.ld\" -Xlinker --gc-sections -Wl,-Map=STM32F4.map" CACHE INTERNAL "")
+set(CMAKE_EXE_LINKER_FLAGS    "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 --specs=nano.specs -T \"${CMAKE_CURRENT_LIST_DIR}/STM32F407VGTx_FLASH.ld\" -Xlinker --gc-sections -Wl,-Map=STM32F4.map" CACHE INTERNAL "")
 
 set(CC_EXECUTABLE_SUFFIX ".elf" CACHE INTERNAL "")
   
