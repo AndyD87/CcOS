d7b5c5a3715588d8d2e1ab6c55de31de1748cbb5
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-06-06 22:27:07 +0200

    Html classes added as same as used on my Webpage

---
+++ CcUtil/CcDocuments/CcHtml/CcHtmlHead.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @page      CcUtil
+ * @subpage   CcHtmlHead
+ *
+ * @page      CcHtmlHead
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web:      http://coolcow.de/projects/CcOS
+ * @par       Language: C++11
+ * @brief     Class CcHtmlHead
+ **/
+#ifndef _CcHtmlHead_H_
+#define _CcHtmlHead_H_
+
+#include "CcBase.h"
+
+/**
+ * @brief Class impelmentation
+ */
+class CcHtmlHead
+{
+public:
+  /**
+   * @brief Constructor
+   */
+  CcHtmlHead();
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~CcHtmlHead();
+};
+
+#endif /* _CcHtmlHead_H_ */
