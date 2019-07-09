7c92b57961e1df136ab23a279fddc5bc366276ca
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-07-09 09:02:11 +0200

    Unused variables on escaped methods

--- CcKernel/Platform/Windows/USB/WinUSBHid.cpp
+++ CcKernel/Platform/Windows/USB/WinUSBHid.cpp
@@ -37,10 +37,10 @@ WinUSBHid::~WinUSBHid()
 
 void WinUSBHid::GetDeviceCapabilities()
 {
+#if !defined(__GNUG__) || __GNUG__ > 4
   PHIDP_PREPARSED_DATA  PreparsedData = {0};
   HIDP_CAPS   hidCaps;          //!< Stored Device-Settings
   //get informations of HID
-#if !defined(__GNUG__) || __GNUG__ > 4
   if (HidD_GetPreparsedData(m_DeviceHandle, &PreparsedData))
   {
     //extract important informations of HID
