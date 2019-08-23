5c00a6d50aa2e7737fff3364e8aaebe63e0e46fc
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2019-08-23 12:38:43 +0200

    Mqtt project initiated

---
+++ CcNetwork/CcMqtt/test/CMqttClientTest.cpp
@@ -0,0 +1,47 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @file
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web:      http://coolcow.de/projects/CcOS
+ * @par       Language: C++11
+ * @brief     Implemtation of class CMqttClientTest
+ */
+#include "CMqttClientTest.h"
+#include "CcKernel.h"
+#include "CcString.h"
+#include "CcMqttClient.h"
+
+CMqttClientTest::CMqttClientTest() :
+  CcTest("CMqttClientTest")
+{
+  appendTestMethod("Test basic CcMqttClient", &CMqttClientTest::testStartStop);
+}
+
+CMqttClientTest::~CMqttClientTest()
+{
+}
+
+bool CMqttClientTest::testStartStop()
+{
+  bool bSuccess = true;
+  CcMqttClient oClient;
+  oClient.start();
+  oClient.stop();
+  return bSuccess;
+}
---
+++ CcNetwork/CcMqtt/test/CMqttClientTest.h
@@ -0,0 +1,54 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @page      CcSslTest
+ * @subpage   CMqttClientTest
+ *
+ * @page      CMqttClientTest
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web:      http://coolcow.de/projects/CcOS
+ * @par       Language: C++11
+ * @brief     Class CMqttClientTest
+ **/
+#ifndef _CMqttClientTest_H_
+#define _CMqttClientTest_H_
+
+#include "CcBase.h"
+#include "CcTest.h"
+
+/**
+ * @brief Class implementation
+ */
+class CMqttClientTest : public CcTest<CMqttClientTest>
+{
+public:
+  /**
+   * @brief Constructor
+   */
+  CMqttClientTest();
+
+  /**
+   * @brief Destructor
+   */
+  ~CMqttClientTest();
+
+private:
+  bool testStartStop();
+};
+
+#endif /* _CMqttClientTest_H_ */
