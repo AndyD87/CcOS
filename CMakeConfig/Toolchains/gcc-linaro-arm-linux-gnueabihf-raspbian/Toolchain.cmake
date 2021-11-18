41f8e012127f74bc74d0c03f5471f04d5db56a55
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2021-11-18 16:47:24 +0100

    Tests with armbian sysroot

--- CMakeConfig/Toolchains/gcc-linaro-arm-linux-gnueabihf-raspbian/Toolchain.cmake
+++ CMakeConfig/Toolchains/gcc-linaro-arm-linux-gnueabihf-raspbian/Toolchain.cmake
@@ -1,6 +1,6 @@
 message("- Crosscompiling Toolchain activated")
 
-ifdef(WIN32)
+if(WIN32)
 
   message(FATAL_ERROR "Windows currently not supported")
 
@@ -8,12 +8,12 @@ else()
 
   set( CMAKE_SYSTEM_NAME  Linux )
   set( LINARO_GCC_DIR "${CC_CACHE_DIR}/Toolchains/gcc-linaro-arm-linux-gnueabihf" )
-  
+
   CcDownloadAndExtract( "linaro-gcc-arm-none-eabi"
                         "${LINARO_GCC_DIR}"
                         "https://releases.linaro.org/archive/13.04/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux.tar.xz")
   CcLoadMakeProgram()
-  
+
   set( LINARO_GCC_BIN_DIR "${LINARO_GCC_DIR}/gcc-linaro-arm-linux-gnueabihf-4.7-2013.04-20130415_linux/bin" )
   # specify the cross compiler
   set(CMAKE_ASM_COMPILER ${LINARO_GCC_BIN_DIR}/arm-linux-gnueabihf-as     CACHE INTERNAL "")
@@ -21,7 +21,7 @@ else()
   set(CMAKE_CXX_COMPILER ${LINARO_GCC_BIN_DIR}/arm-linux-gnueabihf-g++    CACHE INTERNAL "")
   set(CMAKE_AR           ${LINARO_GCC_BIN_DIR}/arm-linux-gnueabihf-ar     CACHE INTERNAL "")
   set(GCC_SIZE           ${LINARO_GCC_BIN_DIR}/arm-linux-gnueabihf-size   CACHE INTERNAL "")
-  
+
   # use sysroot from minimal build at
   #  https://github.com/armbian/build.git
   # echo "PACKAGE_LIST_ADDITIONAL=\"$PACKAGE_LIST_ADDITIONAL libssl-dev\"" > userpatches/lib.config
@@ -34,15 +34,26 @@ else()
   #     KERNEL_ONLY=no \
   #     KERNEL_CONFIGURE=no \
   #
-  
+
   set(SYSROOT_DIR "${CC_CACHE_DIR}/Sysroot/Armbian" )
-  CcDownloadAndExtract( "AmbianSysroot"
+  CcDownloadAndExtract( "ArmbianSysroot"
                         "${SYSROOT_DIR}"
                         "https://coolcow.de/projects/ThirdParty/Armbian/binaries/0.0.0.1/focal-minimal-armhf.7efc0a5812b095319a8270f8c5f2bdd7.tar.lz4"
                         "f5c5236cdc3ada323b8db1f7ca02e7b62a6d56c6")
-  
+
+  foreach(TARGET  EXE SHARED STATIC MODULE)
+  	set(CMAKE_${TARGET}_LINKER_FLAGS_INIT "-L\"${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf\"")
+    set(CMAKE_${TARGET}_LINKER_FLAGS "-L\"${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf\"")
+  	set(CMAKE_${TARGET}_LINKER_FLAGS_INIT "-L\"${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf\"" CACHE INTERNAL "")
+  	set(CMAKE_${TARGET}_LINKER_FLAGS "-L\"${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf\"" CACHE INTERNAL "")
+  endforeach()
+
+  foreach(TARGET  EXE SHARED STATIC MODULE)
+  	message(${CMAKE_${TARGET}_LINKER_FLAGS_INIT})
+  endforeach()
+
   set(CMAKE_FIND_ROOT_PATH ${SYSROOT_DIR})
-  
+
   # search for programs in the build host directories
   set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER             CACHE INTERNAL "")
   # for libraries and headers in the target directories
