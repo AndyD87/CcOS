dedc330557102e8f6bf7aeaafc3bfd331b618b2e
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2021-12-01 06:58:16 +0100

    PortExpandere added

---
+++ CcKernel/Driver/I2C/PortExpander/MCP23017.cpp
@@ -0,0 +1,37 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @file
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web:      https://coolcow.de/projects/CcOS
+ * @par       Language: C++11
+ * @brief     Implemtation of class MCP23017
+ */
+#include "MCP23017.h"
+#include "Devices/ISpi.h"
+#include "Devices/IGpioPin.h"
+#include "CcConversionTables.h"
+
+MCP23017::MCP23017(II2C* pII2CDevice) :
+  m_pII2CDevice(pII2CDevice)
+{
+}
+
+MCP23017::~MCP23017()
+{
+}
