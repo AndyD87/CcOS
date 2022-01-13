ce74e9a0e868dc3ffc2a3358eed40d40e066d949
Andreas Dirmeier <coolcow_ccos@yahoo.com>
2022-01-13 13:25:12 +0100

    Adding service tests

---
+++ CcKernel/test/CSystemServiceTest.cpp
@@ -0,0 +1,48 @@
+/*
+ * This file is part of CcOS.
+ *
+ * CcOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CcOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CcOS.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+/**
+ * @file
+ * @copyright Andreas Dirmeier (C) 2017
+ * @author    Andreas Dirmeier
+ * @par       Web:      https://coolcow.de/projects/CcOS
+ * @par       Language: C++11
+ * @brief     Implemtation of class CSystemServiceTest
+ */
+#include "CSystemServiceTest.h"
+#include "CcServiceSystem.h"
+#include "CcKernel.h"
+
+CSystemServiceTest::CSystemServiceTest() :
+  CcTest("CSystemServiceTest")
+{
+  appendTestMethod("Test list services", &CSystemServiceTest::testServiceListing);
+}
+
+CSystemServiceTest::~CSystemServiceTest()
+{
+}
+
+bool CSystemServiceTest::testServiceListing()
+{
+  bool bRet = false;
+  CcStringList oServices = CcKernel::getServiceSystem().getAllServices();
+  if(oServices.size() > 0)
+  {
+    bRet = true;
+  }
+  return bRet;
+}
